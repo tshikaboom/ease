56c34168ee091c5bfe9074a9649d41ed16e81c26
Nate Stedman <natesm@gmail.com>
2010-06-04 15:24:31 -0400

    Automatically package themes with tar when installing.

---
+++ themes/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "  Archiving themes..."
+
+cd themes
+
+for THEME in `find ./* -maxdepth 0 -type d | sed "s/.\///g"`
+	do
+		echo "    Archiving $THEME to $THEME.easetheme ..."
+		cd $THEME
+		tar -cf ../$THEME.easetheme `ls`
+		cd ..
+	done
+
+echo "  Done archiving themes."
+
