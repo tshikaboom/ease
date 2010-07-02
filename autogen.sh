689473593da99a3d2175ceac468f412583517651
Nate Stedman <natesm@gmail.com>
2010-07-02 15:06:57 -0400

    Build Ease primarily as a library.
    - All source files moved to "libease"
    - Small "main" is the entirety of ease

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="ease"
 REQUIRED_AUTOMAKE_VERSION=1.11
 
-(test -f $srcdir/src/main.vala) || {
+(test -f $srcdir/libease/ease-main.vala) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
